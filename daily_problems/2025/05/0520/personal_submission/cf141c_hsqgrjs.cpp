5e6939b972907ccf6ef14b5a787febd8adf869dd
cxjy <159408499+rjydwlh@users.noreply.github.com>
2025-05-20 19:01:07 +0800

    Create cf141c_hsqgrjs.cpp
    2025-5-20

---
+++ daily_problems/2025/05/0520/personal_submission/cf141c_hsqgrjs.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define line    std::cerr << "LINE :"<< __LINE__ << ":  "
+
+void solve() {
+    int n;
+    std::cin >> n;
+    std::vector<std::pair<std::string, int>> a(n);
+    // std::vector<std::string> s(n);
+
+    for(int i = 0; i < n; i++) {
+        auto& [x, y] = a[i];
+        std::cin >> x >> y;
+
+    }
+
+    std::sort(a.begin(), a.end(), [](auto x, auto y) {
+        return x.second < y.second;
+    });
+    std::vector<std::string> b[n];
+    for (auto [x, y] : a)
+    {
+        b[y].push_back(x);
+    }
+    std::vector<std::pair<std::string, int>> ans;
+    bool ok = 1;
+    std::stack<std::pair<std::string, int>> sum;
+    for(auto x : b[0]) sum.push({x, n});
+    for(int i = 1; i < n; i++) {
+        if(b[i].size() == 0) continue;
+        if(sum.size() + ans.size() < i) {
+            std::cout << -1 << "\n";
+            return ;
+        }
+        while(ans.size() < i) {
+            ans.push_back(sum.top());
+            sum.pop();
+        }
+        for(auto x : b[i]) sum.push({x, n - i});
+    }
+    while(sum.size()) {
+        ans.push_back(sum.top());
+        sum.pop();
+    }
+    for(auto [s, x] : ans) {
+        std::cout << s << " " << x << "\n";
+    }
+}
+
+signed main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+    int t = 1;  
+    // std::cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
